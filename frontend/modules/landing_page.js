b8fdca92bdc7004646b079f63e64b109386bca7e
CrioUser <crio-user@criodo.com>
2023-04-28 16:00:09 +0530

    master

--- frontend/modules/landing_page.js
+++ frontend/modules/landing_page.js
@@ -1,6 +1,7 @@
 import config from "../conf/index.js";
 
 async function init() {
+  console.log(config)
   //Fetches list of all cities along with their images and description
   let cities = await fetchCities();
 
@@ -14,16 +15,36 @@ async function init() {
 
 //Implementation of fetch call
 async function fetchCities() {
-  // TODO: MODULE_CITIES
-  // 1. Fetch cities using the Backend API and return the data
+  try {
+    const response = await fetch(config.backendEndpoint+"/cities");
+    if (!response.ok) {
+      throw new Error(`An error occurred: ${response.status}`);
+    }
+    const cities = await response.json();
+    console.log(cities)
+    return cities;
+  } catch (error) {
+    console.error(error);
+  }
+  
 
 }
 
 //Implementation of DOM manipulation to add cities
 function addCityToDOM(id, city, description, image) {
-  // TODO: MODULE_CITIES
-  // 1. Populate the City details and insert those details into the DOM
-
+ let container = document.createElement("div");
+ container.className="col-6";
+ container.innerHTML=`
+ < a href="pages/adventures/?city=${id}>
+ <div>
+ <div>
+ <p>${city}</p>
+ <p>${description}</p>
+ </div>
+ <img src=${image} alt="#">
+ </div>
+ `
+ document.getElementById("data").appendChild(container);
 }
 
 export { init, fetchCities, addCityToDOM };
