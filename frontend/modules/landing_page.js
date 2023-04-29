35ef578f043b9142e0fb634fda8d86792ec22dce
CrioUser <crio-user@criodo.com>
2023-04-29 22:17:19 +0530

    “ME_QTRIPDYNAMIC_MODULE_CITIES”

--- frontend/modules/landing_page.js
+++ frontend/modules/landing_page.js
@@ -1,14 +1,14 @@
 import config from "../conf/index.js";
 
 async function init() {
-  console.log(config)
+ 
   //Fetches list of all cities along with their images and description
   let cities = await fetchCities();
 
   //Updates the DOM with the cities
   if (cities) {
     cities.forEach((key) => {
-      addCityToDOM(key);
+      addCityToDOM(key.id,key.city,key.description,key.image);
     });
   }
 }
@@ -16,14 +16,10 @@ async function init() {
 //Implementation of fetch call
 async function fetchCities() {
   try {
-    const response = await fetch(config.backendEndpoint+"/cities");
-    if (!response.ok) {
-      throw new Error(`An error occurred: ${response.status}`);
-    }
-    const cities = await response.json();
-    console.log(cities)
-    return cities;
-  } catch (error) {
+    const result= await fetch(config.backendEndpoint+"/cities");
+    const data= await result.json();
+    return data;
+  } catch (e) {
     return null;
   }
   
@@ -31,21 +27,21 @@ async function fetchCities() {
 }
 
 //Implementation of DOM manipulation to add cities
-function addCityToDOM({ id, city, description, image }) {
+function addCityToDOM(id, city, description, image) {
  let container = document.createElement("div");
- container.className="col-12 col-sm-6 col-lg-3 mb-4";
+ container.className="col-6 col-lg-3 mb-4";
  container.innerHTML=`
- <a href="pages/adventures/?city=${id}" id=$"${id}">
-  <div class="tile">
-  <img class="img-responsive" src=${image} alt="#"/>
-   <div class="tile-text text-center">
-     <h5>${city}</h5>
-     <p>${description}</p>
-    </div>
+  <a href="pages/adventures/?city=${id}" id="${id}">
+   <div class="tile">
+      <div class="tile-text text-center">
+       <h5>${city}</h5>
+       <p>${description}</p>
+      </div>
+      <img class="img-responsive" src=${image} alt="#"/>
   </div>
  </a>
  
- `
+ `;
  document.getElementById("data").appendChild(container);
 }
 
